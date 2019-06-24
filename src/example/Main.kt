09f889ef81f29e55d41f3d6bf1e1b4189885580c
NicEastvillage <nicoesterby@gmail.com>
2019-06-24 13:03:27 +0200

    First commit.

---
+++ src/example/Main.kt
@@ -0,0 +1,8 @@
+package example
+
+import dk.aau.cs.d409f19.cellumata.library.CellularAutomata2D
+
+fun main() {
+    println("Hello world!")
+    CellularAutomata2D(100, 100)
+}
---
+++ src/library/CellularAutomata2D.kt
@@ -0,0 +1,3 @@
+package dk.aau.cs.d409f19.cellumata.library
+
+class CellularAutomata2D(val width: Int, val height: Int)
