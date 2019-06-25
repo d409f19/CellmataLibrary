6381a7171546604b7de0a1f44eb9150b06c3fb30
NicEastvillage <nicoesterby@gmail.com>
2019-06-25 21:57:13 +0200

    GridView2D now uses relative coordinates.

--- src/library/GridView2D.kt
+++ src/library/GridView2D.kt
@@ -9,14 +9,16 @@ class GridView2D(
 ) {
     fun get(neighbourhood: Neighbourhood2D): LocalNeighbourhood2D {
         return LocalNeighbourhood2D(neighbourhood.relCoords.map { (rx, ry) ->
-            get(x + rx, y + ry)
+            get(rx, ry)
         })
     }
 
-    fun get(x: Int, y: Int): State {
-        if (ca.horizontalEdgeOption == EdgeOption.FINITE && x !in 0..ca.width) return ca.edgeState!!
-        if (ca.verticalEdgeOption == EdgeOption.FINITE && y !in 0..ca.height) return ca.edgeState!!
-        return grid[x wrap ca.width][y wrap ca.height]
+    fun get(relX: Int, relY: Int): State {
+        val absX = x + relX
+        val absY = y + relY
+        if (ca.horizontalEdgeOption == EdgeOption.FINITE && absX !in 0..ca.width) return ca.edgeState!!
+        if (ca.verticalEdgeOption == EdgeOption.FINITE && absY !in 0..ca.height) return ca.edgeState!!
+        return grid[absX wrap ca.width][absY wrap ca.height]
     }
 
     private infix fun Int.wrap(divisor: Int) = (this % divisor).let { if (it < 0) it + divisor else it }
